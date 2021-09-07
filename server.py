d069f70e8973f7d3202bbe4f22ec82bc7554cfd5
NITHESHWAR11 <67722935+NITHESHWAR11@users.noreply.github.com>
2021-09-07 12:03:42 +0530

    initial commit

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
+
+import trainer
+
+app = Flask(__name__)
+api = Api(app)
+
+class ChatBot(Resource):
+    def post(self):
+        user_input = request.json['user_input']
+        return jsonify({'bot': str(trainer.response(user_input))})
+api.add_resource(ChatBot, "/api/bot/")
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ trainer.py
@@ -0,0 +1,15 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+bot = ChatBot('Norman',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    database_uri='sqlite:///database.sqlite3')
+
+trainer = ChatterBotCorpusTrainer(bot)
+
+trainer.train(
+    './data'
+        )
+def response(user_input):
+        response1 = bot.get_response(user_input)
+        return response1
